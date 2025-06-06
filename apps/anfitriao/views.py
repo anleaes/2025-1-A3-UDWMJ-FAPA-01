c024f82578b76211cf0c7072ad012b65576bcc2a
Erick Marchioro <erickmarchioro.dev@gmail.com>
2025-06-06 16:26:33 -0300

    Anfitriao Views Listar Anfitriao

--- apps/anfitriao/views.py
+++ apps/anfitriao/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import AnfitriaoForm
 from .models import Anfitriao
 
-def add_anfitriao(request):
-    template_name = 'anfitrioes/add_anfitriao.html'
+def add_anfitrioes(request):
+    template_name = 'anfitrioes/add_anfitrioes.html'
     context = {}
     if request.method == 'POST':
         form = AnfitriaoForm(request.POST)
@@ -17,8 +17,8 @@ def add_anfitriao(request):
     return render(request, template_name, context)
 
 
-def edit_anfitriao(request, id_anfitriao):
-    template_name = 'anfitrioes/add_anfitriao.html'
+def edit_anfitrioes(request, id_anfitriao):
+    template_name = 'anfitrioes/add_anfitrioes.html'
     context ={}
     anfitriao = get_object_or_404(anfitriao, id=id_anfitriao)
     if request.method == 'POST':
@@ -28,4 +28,12 @@ def edit_anfitriao(request, id_anfitriao):
             return redirect('anfitrioes:list_anfitrioes')
     form = AnfitriaoForm(instance=anfitriao)
     context['form'] = form
+    return render(request, template_name, context)
+
+def list_anfitrioes(request):
+    template_name = 'anfitrioes/list_anfitrioes.html'
+    categories = Anfitriao.objects.filter()
+    context = {
+        'anfitrioes': anfitrioes
+    }
     return render(request, template_name, context)
