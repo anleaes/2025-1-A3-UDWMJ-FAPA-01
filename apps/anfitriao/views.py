7b7c492be7f3927a0954ea3fa09fbcaef5116964
Erick Marchioro <erickmarchioro.dev@gmail.com>
2025-06-06 16:22:01 -0300

    Anfitriao Views Editar Anfitriao

--- apps/anfitriao/views.py
+++ apps/anfitriao/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import AnfitriaoForm
 from .models import Anfitriao
 
 def add_anfitriao(request):
-    template_name = 'anfitriao/add_anfitriao.html'
+    template_name = 'anfitrioes/add_anfitriao.html'
     context = {}
     if request.method == 'POST':
         form = AnfitriaoForm(request.POST)
@@ -13,5 +13,19 @@ def add_anfitriao(request):
             form.save_m2m()
             return redirect('core:home')
     form = AnfitriaoForm()
+    context['form'] = form
+    return render(request, template_name, context)
+
+
+def edit_anfitriao(request, id_anfitriao):
+    template_name = 'anfitrioes/add_anfitriao.html'
+    context ={}
+    anfitriao = get_object_or_404(anfitriao, id=id_anfitriao)
+    if request.method == 'POST':
+        form = AnfitriaoForm(request.POST, instance=anfitriao)
+        if form.is_valid():
+            form.save()
+            return redirect('anfitrioes:list_anfitrioes')
+    form = AnfitriaoForm(instance=anfitriao)
     context['form'] = form
     return render(request, template_name, context)
