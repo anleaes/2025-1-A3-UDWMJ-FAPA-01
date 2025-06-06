41aa2e252b6494708b1c1ccb3c6e86d40ff8d175
Erick Marchioro <erickmarchioro.dev@gmail.com>
2025-06-06 17:11:58 -0300

    Anfitriao Template Criadas

--- apps/anfitriao/models.py
+++ apps/anfitriao/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Anfitriao(models.Model):
     name = models.CharField('Nome', max_length=50)
     email = models.EmailField('Email', max_length=100)
-    senha = models.CharField ('Senha', max_length=50)
-    telefone = models.CharField('Telefone', max_length=13)
+    password = models.CharField ('Senha', max_length=50)
+    telephone = models.CharField('Telefone', max_length=13)
     biography = models.TextField('Descricao', max_length=300) 
     
     class Meta:
--- apps/anfitriao/urls.py
+++ apps/anfitriao/urls.py
@@ -5,7 +5,7 @@ app_name = 'anfitrioes'
 
 urlpatterns = [
     path('adicionar/', views.add_anfitrioes, name='add_anfitrioes'),
-    path('', views.list_anfitrioes, name='list_categories'),
+    path('', views.list_anfitrioes, name='list_anfitrioes'),
     path('editar/<int:id_anfitriao>/', views.edit_anfitrioes, name='edit_anfitrioes'),
     path('excluir/<int:id_anfitriao>/', views.delete_anfitrioes, name='delete_anfitrioes'),
 ]
