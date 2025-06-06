2aef09c88f1f940c4194c8771b42ebfca6026fc0
Erick Marchioro <erickmarchioro.dev@gmail.com>
2025-06-06 16:00:34 -0300

    Anfitrião Views (Adicionar Anfitriao)

--- apps/anfitriao/views.py
+++ apps/anfitriao/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AnfitriaoForm
+from .models import Anfitriao
 
-# Create your views here.
+def add_anfitriao(request):
+    template_name = 'anfitriao/add_anfitriao.html'
+    context = {}
+    if request.method == 'POST':
+        form = AnfitriaoForm(request.POST)
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.save()
+            form.save_m2m()
+            return redirect('core:home')
+    form = AnfitriaoForm()
+    context['form'] = form
+    return render(request, template_name, context)
