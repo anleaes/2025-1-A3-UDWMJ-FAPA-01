40e2fbd31144f0c0b31ca79ce05eee64c9cb5faf
Erick Marchioro <erickmarchioro.dev@gmail.com>
2025-06-09 14:40:24 -0300

    hospede models criada

--- apps/hospede/models.py
+++ apps/hospede/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Hospede(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    email = models.EmailField('Email', max_length=100)
+    password = models.CharField ('Senha', max_length=50)
+    telephone = models.CharField('Telefone', max_length=13)
+    
+    
+    class Meta:
+        verbose_name = 'Hospede'
+        verbose_name_plural = 'Hospedes'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
+
