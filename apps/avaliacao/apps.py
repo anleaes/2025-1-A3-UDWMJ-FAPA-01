8588832a3844dcb233c842215066c0c8be44db88
Erick Marchioro <erickmarchioro.dev@gmail.com>
2025-06-23 03:45:31 -0300

    Avaliacao criada e configurada

---
+++ apps/avaliacao/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AvaliacaoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'avaliacao'
+    verbose_name = 'avaliacoes'
